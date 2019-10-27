5fc29312c2ccef7a7287ecdc3f062df279bdf5b4
Rizwan-Ijaz <rizwanijaz2@gmail.com>
2019-10-27 15:12:07 +0500

    angular material module imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './angular-material.module';
 
 
 @NgModule({
@@ -13,9 +13,10 @@ import {MatButtonModule} from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
